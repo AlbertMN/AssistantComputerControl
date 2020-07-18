23005bffdfdf3f1d496dac688fcc366894497593
Shelby Marvell <shelby@marvell.cat>
2020-07-17 23:57:08 -0700

    New Xcode Project
    I made a new project of type Preference Pane to see if I can write a control panel and helper daemon to start executing code from OneDrive, Dropbox or iCloud!

---
+++ AssistantComputerControl/AssistantComputerControl.h
@@ -0,0 +1,15 @@
+//
+//  AssistantComputerControl.h
+//  AssistantComputerControl
+//
+//  Created by Shelby Marvell on 7/17/20.
+//  Copyright © 2020 Shelby Marvell. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface AssistantComputerControl : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ AssistantComputerControl/AssistantComputerControl.m
@@ -0,0 +1,17 @@
+//
+//  AssistantComputerControl.m
+//  AssistantComputerControl
+//
+//  Created by Shelby Marvell on 7/17/20.
+//  Copyright © 2020 Shelby Marvell. All rights reserved.
+//
+
+#import "AssistantComputerControl.h"
+
+@implementation AssistantComputerControl
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
